d877fffa6bae736f0906fd2aa77b8832465b9aab
Artyom Erofeev <34557625+ErofeevAA@users.noreply.github.com>
2021-10-27 20:34:27 +1000

    add registration login front screen

--- fefuactivity/ViewController.swift
+++ fefuactivity/ViewController.swift
@@ -1,16 +1,29 @@
-//
-//  ViewController.swift
-//  fefuactivity
-//
-//  Created by Andrew L on 04.10.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    } 
+    
+    @IBAction func OnClickSignIn(_ sender: Any) {
+        let controler = SignInController(nibName: "SignInController", bundle: nil)
+        navigationController?.pushViewController(controler, animated: true)
+    }
+    
+    @IBAction func OnClickSignUp(_ sender: Any) {
+        let controler = SignUpController(nibName: "SignUpController", bundle: nil)
+        navigationController?.pushViewController(controler, animated: true)
     }
 }
 
